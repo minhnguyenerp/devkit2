9afe22d90b5430611740cf9e938ac3d43e8b00bd
Minh Nguyen <minhnguyenerp@gmail.com>
2026-03-23 17:30:54 +0700

    Add putty and superputty

---
+++ Applications/Putty.cs
@@ -0,0 +1,135 @@
+﻿using devkit2.Common;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Text.Json.Nodes;
+
+namespace devkit2.Applications
+{
+    internal sealed class Putty : BaseApplication
+    {
+        public override string Name => "Putty";
+
+        public Putty()
+        {
+            appPath = Path.Combine(BaseApplication.LocalApplicationData, "apps", "putty");
+            if (!Directory.Exists(appPath))
+            {
+                Directory.CreateDirectory(appPath);
+            }
+            base.LoadConfig(appPath);
+            ReloadIcon();
+        }
+
+        public override void ReloadIcon()
+        {
+            try
+            {
+                base.Icon = Icon.ExtractAssociatedIcon(
+                    Path.Combine(appPath, InstalledVersions[0].Value, "PUTTY.EXE")
+                );
+            }
+            catch { }
+        }
+
+        public override bool Valid
+        {
+            get
+            {
+                if (Config != null)
+                    return true;
+                return false;
+            }
+        }
+
+        public override ValueName[] AvailableVersions
+        {
+            get
+            {
+                return new ValueName[]
+                {
+                    new ValueName("0.83", "0.83"),
+                };
+            }
+        }
+
+        public override bool Install(string version, IProgress<DownloadProgress>? progress = null)
+        {
+            string url = string.Empty;
+            string file = string.Empty;
+            switch (version)
+            {
+                case "0.83":
+                    url = "https://the.earth.li/~sgtatham/putty/latest/w64/putty.zip";
+                    file = Path.Combine(Path.GetTempPath(), "putty-0.83.zip");
+                    break;
+            }
+
+            if (url != string.Empty && file != string.Empty)
+            {
+                if (!base.Download(url, file, progress))
+                {
+                    return false;
+                }
+
+                string extractPath = Path.Combine(appPath, version);
+                Directory.CreateDirectory(extractPath);
+                try
+                {
+                    ZipFile.ExtractToDirectory(file, extractPath, true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "DevKit2", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    File.Delete(file);
+                    return false;
+                }
+
+                base.SaveNewVersion(version);
+
+                return true;
+            }
+            return false;
+        }
+
+        public override ValueName[] GetEnvironments(string version)
+        {
+            return new ValueName[] {
+                new ValueName("PATH", Path.Combine(appPath, version)),
+            };
+        }
+
+        public override bool Start(string version, ValueName[] environments, JsonObject? profile = null, string uniqueCode = "")
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = Path.Combine(appPath, version, "PUTTY.EXE");
+            string workingDir = profile?["WorkingDirectory"]?.ToString() ?? string.Empty;
+            if (!string.IsNullOrEmpty(workingDir) && Directory.Exists(workingDir))
+            {
+                psi.WorkingDirectory = workingDir;
+            }
+            psi.UseShellExecute = false;
+            LoadEnvironments(ref psi, environments);
+
+            try
+            {
+                var proc = Process.Start(psi);
+                if (proc != null)
+                {
+                    Sysconf.Instance.AddRunningApplication(new RunningApplication
+                    {
+                        UniqueCode = uniqueCode,
+                        Pid = proc.Id,
+                        Sessionid = proc.SessionId,
+                        ProcessName = proc.ProcessName,
+                        StartTime = proc.StartTime,
+                        ApplicationName = Name,
+                        ApplicationVersion = version,
+                    });
+                    return true;
+                }
+            }
+            catch { return false; }
+            return false;
+        }
+    }
+}
---
+++ Applications/SuperPutty.cs
@@ -0,0 +1,135 @@
+﻿using devkit2.Common;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Text.Json.Nodes;
+
+namespace devkit2.Applications
+{
+    internal sealed class SuperPutty : BaseApplication
+    {
+        public override string Name => "SuperPutty";
+
+        public SuperPutty()
+        {
+            appPath = Path.Combine(BaseApplication.LocalApplicationData, "apps", "superputty");
+            if (!Directory.Exists(appPath))
+            {
+                Directory.CreateDirectory(appPath);
+            }
+            base.LoadConfig(appPath);
+            ReloadIcon();
+        }
+
+        public override void ReloadIcon()
+        {
+            try
+            {
+                base.Icon = Icon.ExtractAssociatedIcon(
+                    Path.Combine(appPath, InstalledVersions[0].Value, $"SuperPuTTY-{InstalledVersions[0].Value}", "SuperPutty.exe")
+                );
+            }
+            catch { }
+        }
+
+        public override bool Valid
+        {
+            get
+            {
+                if (Config != null)
+                    return true;
+                return false;
+            }
+        }
+
+        public override ValueName[] AvailableVersions
+        {
+            get
+            {
+                return new ValueName[]
+                {
+                    new ValueName("1.4.0.9", "1.4.0.9"),
+                };
+            }
+        }
+
+        public override bool Install(string version, IProgress<DownloadProgress>? progress = null)
+        {
+            string url = string.Empty;
+            string file = string.Empty;
+            switch (version)
+            {
+                case "1.4.0.9":
+                    url = "https://superputty.org/wp-content/uploads/2024/04/SuperPuTTY-1.4.0.9.zip";
+                    file = Path.Combine(Path.GetTempPath(), "SuperPuTTY-1.4.0.9.zip");
+                    break;
+            }
+
+            if (url != string.Empty && file != string.Empty)
+            {
+                if (!base.Download(url, file, progress))
+                {
+                    return false;
+                }
+
+                string extractPath = Path.Combine(appPath, version);
+                Directory.CreateDirectory(extractPath);
+                try
+                {
+                    ZipFile.ExtractToDirectory(file, extractPath, true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "DevKit2", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    File.Delete(file);
+                    return false;
+                }
+
+                base.SaveNewVersion(version);
+
+                return true;
+            }
+            return false;
+        }
+
+        public override ValueName[] GetEnvironments(string version)
+        {
+            return new ValueName[] {
+                new ValueName("PATH", Path.Combine(appPath, version, $"SuperPuTTY-{version}")),
+            };
+        }
+
+        public override bool Start(string version, ValueName[] environments, JsonObject? profile = null, string uniqueCode = "")
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = Path.Combine(appPath, version, $"SuperPuTTY-{version}", "SuperPutty.exe");
+            string workingDir = profile?["WorkingDirectory"]?.ToString() ?? string.Empty;
+            if (!string.IsNullOrEmpty(workingDir) && Directory.Exists(workingDir))
+            {
+                psi.WorkingDirectory = workingDir;
+            }
+            psi.UseShellExecute = false;
+            LoadEnvironments(ref psi, environments);
+
+            try
+            {
+                var proc = Process.Start(psi);
+                if (proc != null)
+                {
+                    Sysconf.Instance.AddRunningApplication(new RunningApplication
+                    {
+                        UniqueCode = uniqueCode,
+                        Pid = proc.Id,
+                        Sessionid = proc.SessionId,
+                        ProcessName = proc.ProcessName,
+                        StartTime = proc.StartTime,
+                        ApplicationName = Name,
+                        ApplicationVersion = version,
+                    });
+                    return true;
+                }
+            }
+            catch { return false; }
+            return false;
+        }
+    }
+}
