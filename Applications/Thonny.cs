4e20e5dda3e69246acae94de8b753a88a5fb93d2
Minh Nguyen <minhnguyenerp@gmail.com>
2026-04-05 13:01:47 +0700

    Add Thonny

---
+++ Applications/Thonny.cs
@@ -0,0 +1,140 @@
+﻿using devkit2.Common;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Text.Json.Nodes;
+
+namespace devkit2.Applications
+{
+    internal sealed class Thonny : BaseApplication
+    {
+        public override string Name => "Thonny";
+
+        public Thonny()
+        {
+            appPath = Path.Combine(BaseApplication.LocalApplicationData, "apps", "thonny");
+            if (!Directory.Exists(appPath))
+            {
+                Directory.CreateDirectory(appPath);
+            }
+            base.LoadConfig(appPath);
+            ReloadIcon();
+        }
+
+        public override void ReloadIcon()
+        {
+            try
+            {
+                base.Icon = Icon.ExtractAssociatedIcon(
+                    Path.Combine(appPath, InstalledVersions[0].Value, "thonny.exe")
+                );
+            }
+            catch { }
+        }
+
+        public override bool Valid
+        {
+            get
+            {
+                if (Config != null)
+                    return true;
+                return false;
+            }
+        }
+
+        public override ValueName[] AvailableVersions
+        {
+            get
+            {
+                return new ValueName[]
+                {
+                    new ValueName("4.1.7", "4.1.7"),
+                };
+            }
+        }
+
+        public override bool Install(string version, IProgress<InstallProgress>? progress = null)
+        {
+            string url = string.Empty;
+            string file = string.Empty;
+            switch (version)
+            {
+                case "4.1.7":
+                    url = "https://github.com/thonny/thonny/releases/download/v4.1.7/thonny-4.1.7-windows-portable.zip";
+                    file = Path.Combine(Path.GetTempPath(), "thonny-4.1.7-windows-portable.zip");
+                    break;
+            }
+
+            if (url != string.Empty && file != string.Empty)
+            {
+                if (!base.Download(url, file, progress))
+                {
+                    return false;
+                }
+
+                string extractPath = Path.Combine(appPath, version);
+                Directory.CreateDirectory(extractPath);
+                try
+                {
+                    ZipFile.ExtractToDirectory(file, extractPath, true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "DevKit2", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    File.Delete(file);
+                    return false;
+                }
+
+                base.SaveNewVersion(version);
+
+                return true;
+            }
+            return false;
+        }
+
+        public override ValueName[] GetEnvironments(string version)
+        {
+            return new ValueName[] {
+                new ValueName("PATH", Path.Combine(appPath, version)),
+            };
+        }
+
+        public override bool Start(string version, ValueName[] environments, JsonObject? profile = null, string uniqueCode = "")
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = Path.Combine(appPath, version, "thonny.exe");
+            string workingDir = profile?["WorkingDirectory"]?.ToString() ?? string.Empty;
+            if (!string.IsNullOrEmpty(workingDir) && Directory.Exists(workingDir))
+            {
+                psi.WorkingDirectory = workingDir;
+            }
+            string startupFile = profile?["StartupFile"]?.ToString() ?? string.Empty;
+            if (!string.IsNullOrEmpty(startupFile) && File.Exists(startupFile))
+            {
+                psi.ArgumentList.Add(startupFile);
+            }
+            psi.UseShellExecute = false;
+            LoadEnvironments(ref psi, environments);
+
+            try
+            {
+                var proc = Process.Start(psi);
+                if (proc != null)
+                {
+                    Sysconf.Instance.AddRunningApplication(new RunningApplication
+                    {
+                        UniqueCode = uniqueCode,
+                        Pid = proc.Id,
+                        Sessionid = proc.SessionId,
+                        ProcessName = proc.ProcessName,
+                        StartTime = proc.StartTime,
+                        ApplicationName = Name,
+                        ApplicationVersion = version,
+                    });
+                    return true;
+                }
+            }
+            catch { return false; }
+            return false;
+        }
+    }
+}
