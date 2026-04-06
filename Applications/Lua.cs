9ba728593bc8d4136bc5be59875c8d5df097d54e
Minh Nguyen <minhnguyenerp@gmail.com>
2026-04-06 11:47:06 +0700

    Add LUA

---
+++ Applications/Lua.cs
@@ -0,0 +1,136 @@
+﻿using devkit2.Common;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Text.Json.Nodes;
+
+namespace devkit2.Applications
+{
+    internal sealed class Lua : BaseApplication
+    {
+        public override string Name => "Lua";
+
+        public Lua()
+        {
+            appPath = Path.Combine(BaseApplication.LocalApplicationData, "apps", "lua");
+            if (!Directory.Exists(appPath))
+            {
+                Directory.CreateDirectory(appPath);
+            }
+            base.LoadConfig(appPath);
+            ReloadIcon();
+        }
+
+        public override void ReloadIcon()
+        {
+            try
+            {
+                base.Icon = Icon.ExtractAssociatedIcon(Environment.SystemDirectory + @"\cmd.exe");
+            }
+            catch { }
+        }
+
+        public override bool Valid
+        {
+            get
+            {
+                if (Config != null)
+                    return true;
+                return false;
+            }
+        }
+
+        public override ValueName[] AvailableVersions
+        {
+            get
+            {
+                return new ValueName[]
+                {
+                    new ValueName("5.5.0", "5.5.0"),
+                };
+            }
+        }
+
+        public override bool Install(string version, IProgress<InstallProgress>? progress = null)
+        {
+            string url = string.Empty;
+            string file = string.Empty;
+            switch (version)
+            {
+                case "5.5.0":
+                    url = "https://github.com/minhnguyenerp/devkit2/releases/download/bin1.0.1/lua-5.5.0_Win64_bin.zip";
+                    file = Path.Combine(Path.GetTempPath(), "lua-5.5.0_Win64_bin.zip");
+                    break;
+            }
+
+            if (url != string.Empty && file != string.Empty)
+            {
+                if (!base.Download(url, file, progress))
+                {
+                    return false;
+                }
+                string extractPath = Path.Combine(appPath, version);
+                Directory.CreateDirectory(extractPath);
+                File.WriteAllText(Path.Combine(appPath, version, "lua.bat"),
+@"@echo off
+lua55.exe %*");
+                try
+                {
+                    ZipFile.ExtractToDirectory(file, extractPath, true);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "DevKit2", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    File.Delete(file);
+                    return false;
+                }
+
+                base.SaveNewVersion(version);
+
+                return true;
+            }
+            return false;
+        }
+
+        public override ValueName[] GetEnvironments(string version)
+        {
+            return new ValueName[] {
+                new ValueName("PATH", Path.Combine(appPath, version)),
+            };
+        }
+
+        public override bool Start(string version, ValueName[] environments, JsonObject? profile = null, string uniqueCode = "")
+        {
+            var psi = new ProcessStartInfo();
+            psi.FileName = "cmd.exe";
+            string workingDir = profile?["WorkingDirectory"]?.ToString() ?? string.Empty;
+            if (!string.IsNullOrEmpty(workingDir) && Directory.Exists(workingDir))
+            {
+                psi.WorkingDirectory = workingDir;
+            }
+            psi.UseShellExecute = false;
+            LoadEnvironments(ref psi, environments);
+
+            try
+            {
+                var proc = Process.Start(psi);
+                if (proc != null)
+                {
+                    Sysconf.Instance.AddRunningApplication(new RunningApplication
+                    {
+                        UniqueCode = uniqueCode,
+                        Pid = proc.Id,
+                        Sessionid = proc.SessionId,
+                        ProcessName = proc.ProcessName,
+                        StartTime = proc.StartTime,
+                        ApplicationName = Name,
+                        RuntimeDirectory = workingDir,
+                        ApplicationVersion = version,
+                    });
+                    return true;
+                }
+            }
+            catch { return false; }
+            return false;
+        }
+    }
+}
